e687386b42a2a4df2052a15e0b69517e58e9ffec
Guilherme Rocha <guirochas17@gmail.com>
2021-03-06 23:06:06 -0300

    adicao de projeto base ao repo

--- src/main/java/core/MainCore.java
+++ src/main/java/core/MainCore.java
@@ -1,7 +1,11 @@
 package core;
 
+import javax.swing.JFrame;
+
 public class MainCore {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello world");
+		JFrame myFrame = new mainFrame();
+		myFrame.setVisible(true);
 	}
 }
