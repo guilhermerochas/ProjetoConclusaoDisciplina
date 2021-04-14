d3805410a9764d61a72f3cf3746627007ab5b0b8
Leandro Bergamo <leandro.bergamo@hotmail.com>
2021-04-13 21:07:27 -0300

    feat: adicionando header no request

--- src/main/java/services/LocalizarService.java
+++ src/main/java/services/LocalizarService.java
@@ -2,23 +2,23 @@ package services;
 
 import com.google.gson.Gson;
 import models.Cep;
-import org.jetbrains.annotations.NotNull;
 
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 import java.time.Duration;
 import java.util.Optional;
-import java.net.http.HttpRequest.BodyPublishers;
-import java.net.http.HttpResponse.BodyHandlers;
-import java.util.concurrent.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
 
 public class LocalizarService {
     private final String BASE_URL = "https://backend-asenjo.herokuapp.com";
     private final HttpClient client;
 
-    public LocalizarService(){
+    public LocalizarService() {
         client = HttpClient.newBuilder()
                 .version(HttpClient.Version.HTTP_2)
                 .followRedirects(HttpClient.Redirect.NORMAL)
@@ -27,26 +27,26 @@ public class LocalizarService {
     }
 
     public Future<Optional<HttpResponse<String>>> GET(String url) {
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(url))
-                    .build();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
 
-            CompletableFuture<Optional<HttpResponse<String>>> response  =
-                    client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-                          .thenApply((result) -> Optional.of(result)).exceptionally((err) -> null);
+        CompletableFuture<Optional<HttpResponse<String>>> response =
+                client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                        .thenApply((result) -> Optional.of(result)).exceptionally((err) -> null);
 
-            return response;
+        return response;
     }
 
     public Optional<String> LocalizarInfomacaoCep(String cep) {
         try {
             Cep cepModel = new Cep(cep);
             String cepJson = new Gson().toJson(cepModel);
-
             HttpRequest req = HttpRequest.newBuilder()
-                                .uri(URI.create(BASE_URL + "/cep"))
-                                .POST(BodyPublishers.ofString(cepJson))
-                                .build();
+                    .uri(URI.create(BASE_URL + "/cep"))
+                    .header("Content-Type", "application/json; charset=UTF-8")
+                    .POST(BodyPublishers.ofString(cepJson))
+                    .build();
 
             HttpResponse<String> response = client.send(req, BodyHandlers.ofString());
             return Optional.of(response.body());
