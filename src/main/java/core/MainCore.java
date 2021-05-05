3ddd63f2d37e91d808ec938580cf8ab69ada6d77
Guilherme Rocha <guirochas17@gmail.com>
2021-05-05 17:19:35 -0300

    added development env file

--- src/main/java/core/MainCore.java
+++ src/main/java/core/MainCore.java
@@ -1,6 +1,7 @@
 package core;
 
 import com.google.gson.Gson;
+import io.github.cdimascio.dotenv.Dotenv;
 import models.LocalizacaoResult;
 import services.LocalizarService;
 import ui.MainPanel;
@@ -10,7 +11,14 @@ import java.util.concurrent.CompletableFuture;
 
 
 public class MainCore {
-	public static void main(String[] args) {
-		MainPanel.main(args);
-	}
+    public static void main(String[] args) {
+
+        //Example for loading ENV variables
+        /*
+        Dotenv dotenv = Dotenv.configure().filename("dev.env").load();
+        System.out.println(dotenv.get("VAR"));
+        */
+
+        MainPanel.main(args);
+    }
 }
